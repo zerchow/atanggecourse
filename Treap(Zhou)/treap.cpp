15fbc56680aa1a7fce7b6d7a70acf23813e69536
zerchow <455063040@qq.com>
2014-10-07 21:08:48 +0800

    差BFS和其他方法

--- Treap(Zhou)/treap.cpp
+++ Treap(Zhou)/treap.cpp
@@ -1,8 +1,15 @@
-#include <iostream>
-
-using namespace std;
+#include "treap.h"
 
 int main()
 {
+	Treap * treap = new Treap();
+
+	for (int i = 0; i < 10; ++i)
+		treap->insertNode(i);
+
+	treap->print();
+
+	cout << treap->findMax();
+	cout << treap->findMin();
 	return 0;
 }
