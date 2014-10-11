aae15342d38780f85ebc248df63d6843fa225f13
zerchow <455063040@qq.com>
2014-10-11 19:04:59 +0800

    find and update

--- Treap(Zhou)/treap.cpp
+++ Treap(Zhou)/treap.cpp
@@ -13,9 +13,9 @@ int main()
 
 	cutline();
 
-	cout<< "最大值 ：" << treap->findMax() 
-		<< "，最小值 ：" << treap->findMin() 
-		<< "，根前驱 ：" << treap->rootPred() 
+	cout << "最大值 ：" << treap->findMax()
+		<< "，最小值 ：" << treap->findMin()
+		<< "，根前驱 ：" << treap->rootPred()
 		<< "，根后继 ：" << treap->rootSucc() << endl;
 
 	cutline();
@@ -25,8 +25,8 @@ int main()
 	cutline();
 
 	int del;
-	cout<<"请输入要删除的节点：";
-	cin>>del;
+	cout << "请输入要删除的节点：";
+	cin >> del;
 	treap->deleteNode(del);
 
 	cutline();
@@ -35,9 +35,41 @@ int main()
 
 	cutline();
 
-	cout<< "最大值 ：" << treap->findMax() 
-		<< "，最小值 ：" << treap->findMin() 
-		<< "，根前驱 ：" << treap->rootPred() 
+	cout << "最大值 ：" << treap->findMax()
+		<< "，最小值 ：" << treap->findMin()
+		<< "，根前驱 ：" << treap->rootPred()
+		<< "，根后继 ：" << treap->rootSucc() << endl;
+
+	cutline();
+
+	treap->printVertical();
+
+	cutline();
+
+	int fin;
+	cout << "请输入要查找的节点：";
+	cin >> fin;
+	treap->findNode(fin);
+
+	cutline();
+
+	int upd;
+	cout << "将查找的节点更新为：";
+	cin >> upd;
+	if (treap->updateNode(fin, upd))
+		cout << "更新成功！" << endl;
+	else
+		cout << "更新失败！" << endl;
+
+	cutline();
+
+	treap->printHorizontal();
+
+	cutline();
+
+	cout << "最大值 ：" << treap->findMax()
+		<< "，最小值 ：" << treap->findMin()
+		<< "，根前驱 ：" << treap->rootPred()
 		<< "，根后继 ：" << treap->rootSucc() << endl;
 
 	cutline();
@@ -46,5 +78,5 @@ int main()
 
 	system("pause");
 
-	return 0;//test
+	return 0;
 }
